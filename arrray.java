808d33987341e70f18195b1766e3f5ecf60680ae
deepuu05 <deepanshu201204@gmail.com>
2024-06-26 23:59:51 +0530

    Create arrray.java

---
+++ arrray.java
@@ -0,0 +1,24 @@
+// largest number of an array
+import java.util.Scanner;
+class HelloWorld {
+    void Arr(){
+    int age[]={1,3,5,6};
+    int ans=0;
+   for(int i=0;i< age.length ;i++){
+       if(age[i]>sum){
+           ans=age[i];
+       }
+   }
+          System.out.println(ans);
+
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+       // System.out.println("hello world");
+        HelloWorld obj= new HelloWorld();
+        //.age=20;
+    //System.out.println("the age is "+obj.age);
+    obj.Arr();
+    }
+}
